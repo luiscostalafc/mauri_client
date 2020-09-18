8442e854bd6eaa8c2c3d0266d72f6733c4befaea
luiscostalafc <luiscostalafc@gmail.com>
2020-09-17 21:26:53 -0300

    Create Redux in Project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import ThemeContainer from '../../src/contexts/theme/ThemeContainer'
+import { Provider } from 'react-redux'
 import AppProvider from '../hooks'
 import {CookiesProvider} from 'react-cookie'
 import { AppProps } from 'next/app'
+
+import store from '../store'
 
 import GlobalStyle from '../styles/global'
 
@@ -10,11 +13,13 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
 
     <ThemeContainer>
+      <Provider store={store}>
       <CookiesProvider>
        <AppProvider>
       <Component {...pageProps} />
        </AppProvider>
        </CookiesProvider>
+       </Provider>
        < GlobalStyle />
     </ThemeContainer>
 
