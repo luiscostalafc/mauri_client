ad96532952acee7df4993a8f8b0740ee9e5ad517
EmersonBraun <emersonfbraun@gmail.com>
2021-01-12 12:15:37 -0300

    feat: create login and admin panel pages

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,25 +1,141 @@
-import React from 'react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { TextField } from '@material-ui/core'
+import React, { useState } from 'react'
+import DataTable from 'react-data-table-component'
 import Template from '../../components/Template'
-import AdminMenu from '../../components/AdminMenu'
 import { get } from '../../services/api'
 
 
+const moduleName = 'users'
 export async function getStaticProps() {
-  const response = await get('users', true, true)
+  const response = await get(moduleName)
   return {
     props: {
-      data: response
+      data: response,
     },
   }
 }
 
-export default function Index() {
+export default function Index({ data }: any) {
+  const [dataVal, setData] = useState(data)
+
+  const FilterComponent = ({ filterText, onFilter, onClear }) => (
+    <>
+      <TextField id="search" type="text" placeholder="Filtro por nome" aria-label="Search Input" value={filterText} onChange={onFilter} />
+      <Button type="button" onClick={onClear}>X</Button>
+    </>
+  );
+
+  function convertArrayOfObjectsToCSV(array) {
+    let result;
+  
+    const columnDelimiter = ',';
+    const lineDelimiter = '\n';
+    const keys = Object.keys(data[0]);
+  
+    result = '';
+    result += keys.join(columnDelimiter);
+    result += lineDelimiter;
+  
+    array.forEach(item => {
+      let ctr = 0;
+      keys.forEach(key => {
+        if (ctr > 0) result += columnDelimiter;
+  
+        result += item[key];
+        
+        ctr++;
+      });
+      result += lineDelimiter;
+    });
+  
+    return result;
+  }
+
+  function downloadCSV(array) {
+    const link = document.createElement('a');
+    let csv = convertArrayOfObjectsToCSV(array);
+    if (csv == null) return;
+  
+    const filename = 'export.csv';
+  
+    if (!csv.match(/^data:text\/csv/i)) {
+      csv = `data:text/csv;charset=utf-8,${csv}`;
+    }
+  
+    link.setAttribute('href', encodeURI(csv));
+    link.setAttribute('download', filename);
+    link.click();
+  }
+  
+  
+  const Export = ({ onExport }) => (
+    <Button onClick={e => onExport(e.target.value)}>Export</Button>
+  );
+
+  const columns = [
+    { name: 'Pedido (data/hs)', selector: 'name', sortable: true, },
+    { name: 'Faturado (data/hs)', selector: 'username', sortable: true, },
+    { name: 'Nº Pedido', selector: 'activity', sortable: true, },
+    { name: 'Cliente', selector: 'complete_name', sortable: true, },
+    { name: 'Forma de Envio', selector: 'email', sortable: true, },
+    { name: 'Fornecedor', selector: 'rg', sortable: true, },
+    { name: 'SKU Código Fornecedor', selector: 'cpf_cnpj', sortable: true, },
+    { name: 'Marca', selector: 'nick', sortable: true, },
+    { name: 'MPM Código Marca', selector: 'is_provider', sortable: true, },
+    { name: 'Qte Vendida', selector: 'inactive', sortable: true, },
+    { name: 'Preço Venda', selector: 'inactive', sortable: true, },
+    { name: 'Preço Custo', selector: 'inactive', sortable: true, },
+    { name: 'Margem', selector: 'inactive', sortable: true, },
+    { name: 'Descrição título do produto', selector: 'inactive', sortable: true, },
+  ]
+
+  const [filterText, setFilterText] = React.useState('');
+  const [resetPaginationToggle, setResetPaginationToggle] = React.useState(false);
+  const filteredItems = dataVal.filter(item => item.name && item.name.toLowerCase().includes(filterText.toLowerCase()));
+
+  const subHeaderComponentMemo = React.useMemo(() => {
+    const handleClear = () => {
+      if (filterText) {
+        setResetPaginationToggle(!resetPaginationToggle);
+        setFilterText('');
+      }
+    };
+
+    return <FilterComponent onFilter={e => setFilterText(e.target.value)} onClear={handleClear} filterText={filterText} />;
+  }, [filterText, resetPaginationToggle])
+
+  const actionsMemo = React.useMemo(() => (downloadCSV(data)), []);
 
   return (
     <Template
-    content={<div>Index</div>}
-    slider={<AdminMenu/>}
-    group={<></>}
+    content={
+      <>
+      <ButtonGroup variant="outline" spacing="6" >
+        <Button borderRadius={5} color="white" size="lg" background="tomato" padding={20}>Preparando</Button>
+        <Button borderRadius={5} color="white" size="lg" background="tomato" padding={20}>Pronto para enviar</Button>
+        <Button borderRadius={5} color="white" size="lg" background="tomato" padding={20}>Em Trânsito</Button>
+        <Button borderRadius={5} color="white" size="lg" background="tomato" padding={20}>Canceladas</Button>
+        <Button borderRadius={5} color="white" size="lg" background="tomato" padding={20}>Concluídas</Button>
+        <Button borderRadius={5} color="white" size="lg" background="tomato" padding={20}>Garantia</Button>
+        <Button borderRadius={5} color="white" size="lg" background="tomato" padding={20}>Importar Pedidos</Button>
+        <Button borderRadius={5} color="white" size="lg" background="tomato" padding={20} onClick={actionsMemo}>Exportar Pedidos</Button>
+        <Button borderRadius={5} color="white" size="lg" background="tomato" padding={20}>Imprimir Pedido</Button>
+        <Button borderRadius={5} color="white" size="lg" background="tomato" padding={20}>Formato etiqueta</Button>
+      </ButtonGroup>
+      <DataTable
+        title="Painel Administrativo"
+        columns={columns}
+        data={filteredItems}
+        pagination
+        paginationResetDefaultPage={resetPaginationToggle} 
+        subHeader
+        subHeaderComponent={subHeaderComponentMemo}
+        selectableRows
+        persistTableHead
+      />
+      </>
+    }
     />
   )
 }
---
+++ src/pages/login.tsx
@@ -0,0 +1,53 @@
+import { Avatar, Box, Button } from '@chakra-ui/core'
+import { Center, Container, Text } from "@chakra-ui/react"
+import Link from 'next/link'
+import React from 'react'
+
+
+export default function Login() {
+
+  return (
+    <Container>
+
+      <Box as="button" borderRadius="xs" w="100%" height="100%" padding={50}>
+        <Center>
+        <Text fontSize="50px" color="tomato">
+          Login
+        </Text>
+        </Center>
+      </Box>
+
+      <Link href="/users/sign-in">
+        <Box as="button" borderRadius="lg" w="50%" height="100%" borderWidth="1px">
+          <Box w="100%" height="50px" bg="tomato">
+            <Avatar size="2xl" bg="teal.500" />
+          </Box>
+          <Box w="100%" padding={50}>
+            <Text fontSize="25px">Já tenho cadastro</Text>
+            <Text fontSize="20px">Informe seu e-mail ou CPF/CNPJ para se autenticar</Text>
+            
+              <Button color="white" size="lg" background="tomato" padding={20}>
+                ACESSAR
+              </Button>
+          </Box>
+        </Box>
+      </Link>
+
+      <Link href="/users/sign-up">
+        <Box as="button" borderRadius="xs" w="50%" height="100%" borderWidth="1px">
+          <Box w="100%" height="50px" bg="grey">
+            <Avatar size="2xl" bg="teal.500" />
+          </Box>
+          <Box w="100%" padding={50}>
+            <Text fontSize="25px">Sou novo cliente</Text>
+            <Text fontSize="20px">O cadastro em nossa loja é simples e rápido</Text>
+              <Button color="white" size="lg" background="grey" padding={20}>
+                CRIAR CONTA
+              </Button>
+          </Box>
+        </Box>
+      </Link>
+
+    </Container>
+  )
+}
