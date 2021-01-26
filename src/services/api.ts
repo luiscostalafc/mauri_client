ebb4c71e4b06d818ffc9f756863a36890033f897
luiscostalafc <luiscostalafc@gmail.com>
2021-01-26 08:11:12 -0300

    feat: include .env.example

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,68 +1,71 @@
-import axios from 'axios'
-import Cookies from 'js-cookie'
-import { useToast } from '../hooks/toast'
-import PrettyLog from '@emersonbraun/pretty-log'
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import PrettyLog from '@emersonbraun/pretty-log';
+import { useToast } from '../hooks/toast';
 
 const completeURL = (URL: string) => {
-  const cleanURL = URL.charAt(0) === '/' ? URL.slice(1, URL.length) : URL
-  return `http://127.0.0.1:3333/api/${cleanURL}`
-}
+  const cleanURL = URL.charAt(0) === '/' ? URL.slice(1, URL.length) : URL;
+  return `${process.env.REACT_APP_API_URL}/api/${cleanURL}`;
+};
 
 declare type Headers = {
-  Accept: string,
-  'Content-Type': 'application/json' | 'multipart/form-data',
-  Authorization: string | null
-}
+  Accept: string;
+  'Content-Type': 'application/json' | 'multipart/form-data';
+  Authorization: string | null;
+};
 
 const headers = {
   Accept: 'application/json',
   'Content-Type': 'application/json',
-  Authorization: null
-} as Headers
+  Authorization: null,
+} as Headers;
 
 function successNotify(msg: string) {
-  const { addToast } = useToast()
+  const { addToast } = useToast();
   return addToast({
     type: 'success',
     title: msg,
-  })
+  });
 }
 
 function errorNotify(msg: string) {
-  const { addToast } = useToast()
+  const { addToast } = useToast();
   return addToast({
     type: 'error',
-    title: msg
-  })
+    title: msg,
+  });
 }
 
-function getToken () {
-  const token = Cookies.get('@Liconnection:token')
-  return token ? `Bearer ${String(token)}` : null
+function getToken() {
+  const token = Cookies.get('@Liconnection:token');
+  return token ? `Bearer ${String(token)}` : null;
 }
 
-function setHeaders (file = false) {
-  headers['Content-Type'] = file ? 'multipart/form-data' : 'application/json'
-  headers.Authorization = getToken()
-  return headers
+function setHeaders(file = false) {
+  headers['Content-Type'] = file ? 'multipart/form-data' : 'application/json';
+  headers.Authorization = getToken();
+  return headers;
 }
 
-function showNotify (response: { headers: { message: string }; status: number }) {
+function showNotify(response: {
+  headers: { message: string };
+  status: number;
+}) {
   if (response?.headers?.message) {
     if (response.status < 200 || response.status > 299) {
-      errorNotify(response.headers.message)
+      errorNotify(response.headers.message);
     } else {
-      successNotify(response.headers.message)
+      successNotify(response.headers.message);
     }
   }
 }
 
-function redirectIfNotLogged (response: { status: number }) {
-  if (response.status === 403) window.location.replace('/login')
-  return true
+function redirectIfNotLogged(response: { status: number }) {
+  if (response.status === 403) window.location.replace('/login');
+  return true;
 }
 
-function logResponse (response: any) {
+function logResponse(response: any) {
   const mainData = {
     method: response.config.method,
     path: response.request.path,
@@ -70,74 +73,84 @@ function logResponse (response: any) {
     returntype: response.headers.returntype,
     message: response.headers.message,
     'data (count)': response.data.length || 0,
-  }
-  PrettyLog.success(`Response ${mainData.path}:`)
-  console.table(mainData)
+  };
+  PrettyLog.success(`Response ${mainData.path}:`);
+  console.table(mainData);
 }
 
-function setResponse (response: any, silent = false, debug = false) {
-  redirectIfNotLogged (response)
-  if (debug) logResponse (response)
-  if (!silent) showNotify(response)
-  return response.data
+function setResponse(response: any, silent = false, debug = false) {
+  redirectIfNotLogged(response);
+  if (debug) logResponse(response);
+  if (!silent) showNotify(response);
+  return response.data;
 }
 
-export async function get (URL: string, silent = true, debug = false) {
-  const headers = setHeaders()
-  if (debug) console.time('⌚️ time to get request')
+export async function get(URL: string, silent = true, debug = false) {
+  const headers = setHeaders();
+  if (debug) console.time('⌚️ time to get request');
   try {
-    const response = await axios.get(completeURL(URL), { headers })
-    if (debug) console.timeEnd('⌚️ time to get request')
-    return setResponse(response, silent, debug)
+    const response = await axios.get(completeURL(URL), { headers });
+    if (debug) console.timeEnd('⌚️ time to get request');
+    return setResponse(response, silent, debug);
   } catch (e) {
-    if (debug) console.timeEnd('⌚️ time to get request')
-    PrettyLog.error(`Error to get ${URL}`, e)
+    if (debug) console.timeEnd('⌚️ time to get request');
+    PrettyLog.error(`Error to get ${URL}`, e);
   }
 }
 
-export async function post (URL: string, data: unknown, file = false, silent = false, debug = false) {
-  const headers = setHeaders(file)
-  if (debug) console.time('⌚️ time to post request')
+export async function post(
+  URL: string,
+  data: unknown,
+  file = false,
+  silent = false,
+  debug = false,
+) {
+  const headers = setHeaders(file);
+  if (debug) console.time('⌚️ time to post request');
   try {
-    const response = await axios.post(completeURL(URL), data, { headers })
-    if (debug) console.timeEnd('⌚️ time to post request')
-    return setResponse(response, silent, debug)
+    const response = await axios.post(completeURL(URL), data, { headers });
+    if (debug) console.timeEnd('⌚️ time to post request');
+    return setResponse(response, silent, debug);
   } catch (e) {
-    if (debug) console.timeEnd('⌚️ time to post request')
-    PrettyLog.error(`Error to post ${URL}`, e)
+    if (debug) console.timeEnd('⌚️ time to post request');
+    PrettyLog.error(`Error to post ${URL}`, e);
   }
 }
 
-export async function put (URL: string, data: unknown, silent = false, debug = false) {
-  const headers = setHeaders()
-  if (debug) console.time('⌚️ time to put request')
+export async function put(
+  URL: string,
+  data: unknown,
+  silent = false,
+  debug = false,
+) {
+  const headers = setHeaders();
+  if (debug) console.time('⌚️ time to put request');
   try {
-    const response = await axios.put(completeURL(URL), data, { headers })
-    if (debug) console.timeEnd('⌚️ time to put request')
-    return setResponse(response, silent, debug)
+    const response = await axios.put(completeURL(URL), data, { headers });
+    if (debug) console.timeEnd('⌚️ time to put request');
+    return setResponse(response, silent, debug);
   } catch (e) {
-    if (debug) console.timeEnd('⌚️ time to put request')
-    PrettyLog.error(`Error to put ${URL}`, e)
+    if (debug) console.timeEnd('⌚️ time to put request');
+    PrettyLog.error(`Error to put ${URL}`, e);
   }
 }
 
-export async function deleteData (URL: string, silent = false, debug = false) {
-  const headers = setHeaders()
-  if (debug) console.time('⌚️ time to delete request')
+export async function deleteData(URL: string, silent = false, debug = false) {
+  const headers = setHeaders();
+  if (debug) console.time('⌚️ time to delete request');
   try {
-    const response = await axios.delete(completeURL(URL), { headers })
-    if (debug) console.timeEnd('⌚️ time to delete request')
-    return setResponse(response, silent, debug)
+    const response = await axios.delete(completeURL(URL), { headers });
+    if (debug) console.timeEnd('⌚️ time to delete request');
+    return setResponse(response, silent, debug);
   } catch (e) {
-    if (debug) console.timeEnd('⌚️ time to delete request')
-    PrettyLog.error(`Error to delete ${URL}`, e)
+    if (debug) console.timeEnd('⌚️ time to delete request');
+    PrettyLog.error(`Error to delete ${URL}`, e);
   }
 }
 
-
- /* ------ original ------*/
+/* ------ original ------*/
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
-})
+  baseURL: process.env.REACT_APP_API_URL,
+});
 
-export default api
+export default api;
