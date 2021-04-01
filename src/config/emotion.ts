e995fea8b0da1f77fb083d33d719a94286f2ae1a
EmersonBraun <emersonfbraun@gmail.com>
2021-04-01 12:42:33 -0300

    fix: solve styles

--- src/config/emotion.ts
+++ src/config/emotion.ts
@@ -1,8 +1,4 @@
+import styled, { CreateStyled } from '@emotion/styled';
 import theme from '../styles/theme';
 
-type ThemeType = typeof theme;
-
-declare module "@emotion/react" {
-  export interface Theme extends ThemeType {}
-}
-// export default styled as CreateStyled<typeof theme>;
+export default styled as CreateStyled<typeof theme>;
